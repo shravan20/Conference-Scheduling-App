705001f5106d0210a5caddf6a447286f51717666
shravan20 <mrshravankumarb@gmail.com>
2021-04-13 14:12:44 +0530

    :sparkles: Added Sessions Model

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,4 +1,20 @@
 package com.pluralsight.conferencedemo.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity(name = "sessions")
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
 }
