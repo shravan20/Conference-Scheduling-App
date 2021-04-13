95dd5762b0429f95df218adf3efaea09ac3770fb
shravan20 <mrshravankumarb@gmail.com>
2021-04-13 14:22:23 +0530

    :sparkles: Added Speakers Model with Getters/Setters

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Speakers.java
@@ -0,0 +1,27 @@
+package com.pluralsight.conferencedemo.models;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "speakers")
+@Getter
+@Setter
+public class Speakers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long speaker_id;
+
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+
+}
