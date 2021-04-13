ca2a939dfd08d71d26ed287686e77d8c841dea49
shravan20 <mrshravankumarb@gmail.com>
2021-04-13 14:14:00 +0530

    :sparkles: Updated Sessions Model | Added Getter/Setter

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,6 +1,9 @@
 package com.pluralsight.conferencedemo.models;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +11,8 @@ import javax.persistence.Id;
 
 
 @Entity(name = "sessions")
+@Getter
+@Setter
 public class Session {
 
     @Id
