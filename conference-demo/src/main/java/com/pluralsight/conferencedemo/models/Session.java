f78a35ef00a719278b62bb63ba9b9e488c28001d
shravan20 <mrshravankumarb@gmail.com>
2021-04-13 14:28:26 +0530

    :sparkles: Added ManyToMany Mapping to build the relationship

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -4,10 +4,8 @@ package com.pluralsight.conferencedemo.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 
 @Entity(name = "sessions")
@@ -21,5 +19,14 @@ public class Session {
     private String session_name;
     private String session_description;
     private Integer session_length;
+
+    @ManyToMany
+    @JoinTable(
+            name="session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+    private List<Speaker> speakers;
+
 
 }
